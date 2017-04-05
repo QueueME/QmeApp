7bf123d80245ed16d8683d69542e921f6a0505bf
andykamp <akampenes@gmail.com>
2017-04-05 16:59:04 +0200

    test

---
+++ app/src/test/java/com/example/queueme/WelcomeActivityStudentTest.java
@@ -0,0 +1,68 @@
+package com.example.queueme;
+
+
+import android.content.Intent;
+
+import com.example.queueme.Infoscreens.WelcomeActivityStudent;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.Shadows;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowActivity;
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Created by Marius on 04.04.2017.
+ */
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
+
+public class WelcomeActivityStudentTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+
+    @Test
+    public void welcomeActivityStudentTest(){
+
+
+        WelcomeActivityStudent mMainActivity = Robolectric.setupActivity(WelcomeActivityStudent.class);
+        mMainActivity.findViewById(R.id.btn_skip).performClick();
+        mMainActivity.findViewById(R.id.btn_next).performClick();
+
+        Intent expectedIntent = new Intent(mMainActivity, ChooseSubjectStud.class);
+
+        ShadowActivity shadowActivity = Shadows.shadowOf(mMainActivity);
+        Intent actualIntent = shadowActivity.getNextStartedActivity();
+
+        assertTrue(actualIntent.filterEquals(expectedIntent));
+
+
+
+    }
+
+
+
+
+
+
+    @Test
+    public void onCreate() throws Exception {
+
+    }
+
+}
